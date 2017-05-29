262564c7d6e24c9a536669d7fe157a7dbf8d6f6f
Matthieu Tourne <matthieu.tourne@gmail.com>
2017-05-29 16:55:55 -0700

    Only pick digits between 1 and 6
    todo (mtourne): make it configurable

--- src/secretGenerator.js
+++ src/secretGenerator.js
@@ -3,10 +3,17 @@
 class SecretGenerator {
   generateWithLength(length) {
     var digits = [];
-    while (length --> 0) {
-      digits.push(Math.floor(Math.random() * 10))
+    while (length > 0) {
+      var rand = Math.floor(Math.random() * 10)
+      if (rand <= 0 || rand > 6) {
+        continue
+      }
+      digits.push(rand);
+      length = length - 1;
     }
-    return digits.join('');
+    var res = digits.join('');
+    console.log(res)
+    return res
   }
 }
 
